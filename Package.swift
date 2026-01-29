c1fffbaec4f3d173b66bd70015e8e7d73cd1efb2
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-29 21:25:24 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,55 @@
+// swift-tools-version: 6.2
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-parser-machine-primitives",
+    platforms: [
+        .macOS(.v26),
+        .iOS(.v26),
+        .tvOS(.v26),
+        .watchOS(.v26),
+        .visionOS(.v26)
+    ],
+    products: [
+        .library(
+            name: "Parser Machine Primitives",
+            targets: ["Parser Machine Primitives"]
+        ),
+    ],
+    dependencies: [
+        .package(path: "../swift-parser-primitives"),
+        .package(path: "../swift-input-primitives"),
+        .package(path: "../swift-stack-primitives"),
+        .package(path: "../swift-slab-primitives"),
+        .package(path: "../swift-identity-primitives"),
+        .package(path: "../swift-ownership-primitives"),
+        .package(path: "../swift-machine-primitives"),
+    ],
+    targets: [
+        .target(
+            name: "Parser Machine Primitives",
+            dependencies: [
+                .product(name: "Parser Primitives", package: "swift-parser-primitives"),
+                .product(name: "Input Primitives", package: "swift-input-primitives"),
+                .product(name: "Stack Primitives", package: "swift-stack-primitives"),
+                .product(name: "Slab Primitives", package: "swift-slab-primitives"),
+                .product(name: "Identity Primitives", package: "swift-identity-primitives"),
+                .product(name: "Ownership Primitives", package: "swift-ownership-primitives"),
+                .product(name: "Machine Primitives", package: "swift-machine-primitives")
+            ]
+        ),
+    ],
+    swiftLanguageModes: [.v6]
+)
+
+for target in package.targets where ![.system, .binary, .plugin, .macro].contains(target.type) {
+    let settings: [SwiftSetting] = [
+        .enableUpcomingFeature("ExistentialAny"),
+        .enableUpcomingFeature("InternalImportsByDefault"),
+        .enableUpcomingFeature("MemberImportVisibility"),
+        .enableExperimentalFeature("Lifetimes"),
+        .strictMemorySafety()
+    ]
+    target.swiftSettings = (target.swiftSettings ?? []) + settings
+}
